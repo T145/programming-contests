c75a54c0d8f87cdfbad74b3579567a03a1e80369
T145 <T145@protonmail.com>
2018-10-22 22:24:57 -0400

    Added bisecting frogs solution

---
+++ 1995/bisectingfrogs.py
@@ -0,0 +1,11 @@
+
+# f(x) = y = x^3 + 4x^2 - 10
+# 1-2 inches per day
+# f(2) = 14, therefore best dist is 7
+
+def ftn(x):
+	return x**3 + 4 * (x**2) - 10
+
+#print(ftn(1.725)) = 7.035453125000004
+
+print('The optimal amount of rainfall for frog viewing is ~1.725 inches.')
