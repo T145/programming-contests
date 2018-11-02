40cbb6b0ad8643e49226ee7bd5634e28d0089fa4
T145 <T145@protonmail.com>
2018-11-01 21:09:29 -0400

    Added 1998 problem dirs

--- 1998/P6 Y2K, U2K/y2k.py
+++ 1998/P6 Y2K, U2K/y2k.py
@@ -8,5 +8,5 @@ def normalize_year(s):
 
 with open('prob6.in') as f:
 	for line in f:
-		date = datetime.datetime.strptime(line.replace('\n', ''), '%y%j').date()
+		date = datetime.datetime.strptime(line.strip(), '%y%j').date()
 		print('{0}/{1}'.format(date.strftime('%m/%d'), normalize_year(date.strftime('%Y'))))
