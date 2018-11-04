261ba570526b4dc9eff53be400e6e28839899a5f
T145 <T145@protonmail.com>
2018-11-03 23:11:52 -0400

    Added Secret Club Name solution

---
+++ 2016/P1 Secret Club Name/club.py
@@ -0,0 +1,14 @@
+def comp_club(s, is_vowel):
+	s = list(s)
+	if is_vowel:
+		locs = [i for i, c in enumerate(s) if c in 'AEIOU']
+	else:
+		locs = [i for i, c in enumerate(s) if c not in 'AEIOU']
+	for l, r in zip(locs[:int(len(locs)/2)], reversed(locs)):
+		s[l], s[r] = s[r], s[l]
+	return ''.join(s)
+
+line = input()
+while line != 'LAST':
+	print('{0}:{1}'.format(line, comp_club(comp_club(line, False), True)))
+	line = input()
