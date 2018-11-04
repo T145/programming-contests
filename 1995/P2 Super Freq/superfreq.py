93755a321b2a0944e3658958a4d968f31484e856
T145 <T145@protonmail.com>
2018-11-03 23:11:36 -0400

    Optimized Super Freq

--- 1995/P2 Super Freq/superfreq.py
+++ 1995/P2 Super Freq/superfreq.py
@@ -1,13 +1,11 @@
-import re
 from collections import Counter
 
 with open('prob2.in') as f:
 	for line in f:
-		s = re.sub(r"[^a-z]+", '', line.lower())
-		d, c = Counter(s), 0
-		for e in d:
-			if d[e] / len(line) > 0.15:
-				print('{0} is a super freq.'.format(e.upper()))
-				c += 1
-		if (c == 0):
+		counts = Counter(c for c in line.upper() if 'A' <= c <= 'Z')
+		super_freqs = [c for c, n in counts.items() if n / len(line) > 0.15]
+		if not super_freqs:
 			print('There are no super freqs.')
+		else:
+			for c in super_freqs:
+				print('{0} is a super freq.'.format(c))
